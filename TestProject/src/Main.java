b2f7325ec057aacda237346471f0c9a7d552f560
Raiski Ilya <raiskiilya@gmail.com>
2023-09-26 15:19:45 +0300

    change

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -1,3 +1,5 @@
+import entity.Person;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -5,7 +7,7 @@ public class Main {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-
+        Person person = new Person();
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
